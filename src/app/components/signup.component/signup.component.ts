f6a437722163f7ade1373ca14fcb8a0aa7f3cdfc
Mohsin <mmohsinkhalid@outlook.com>
2017-01-25 21:20:43 +0500

    Signedup student and companies using firebase

--- src/app/components/signup.component/signup.component.ts
+++ src/app/components/signup.component/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire} from 'angularfire2'
 
 @Component({
   selector: 'login',
@@ -6,12 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  userObj = {};
-  companyObj = {};
-  signupUser(){
-    console.log(this.userObj);
+  userObj = {
+    username: "",
+    age: "",
+    collegeName: "",
+    lastDegree: "",
+    marksObtain: "",
+    password: "",
+    role: ""
+  };
+  companyObj = {
+    email: "",
+    companyName: "",
+    password: "",
+    role: ""
+  };
+
+  usersRef;
+
+  constructor(private af: AngularFire) {
+    this.usersRef = af.database.list("/users");
   }
-  signupCompany(){
-    console.log(this.companyObj);
+
+  signupUser() {
+    if (this.userObj.username.trim() != "" && this.userObj.age.trim() != "" && this.userObj.collegeName.trim() != "" &&
+      this.userObj.lastDegree.trim() != "" && this.userObj.marksObtain.trim() != "" && this.userObj.password.trim() != "") {
+      console.log(this.userObj);
+      this.userObj.role = "student";
+      this.usersRef.push(this.userObj);
+      this.userObj = {
+        username: "",
+        age: "",
+        collegeName: "",
+        lastDegree: "",
+        marksObtain: "",
+        password: "",
+        role: ""
+      };
+      alert("Successfully user created ");
+    }
+    else {
+      alert("Please fill all fields");
+    }
+  }
+
+  signupCompany() {
+    if (this.companyObj.email.trim() != "" && this.companyObj.companyName.trim() != "" && this.companyObj.password.trim() != "") {
+      this.companyObj.role = "company";
+      this.usersRef.push(this.companyObj);
+
+      console.log(this.companyObj);
+      this.companyObj = {
+        email: "",
+        companyName: "",
+        password: "",
+        role: ""
+      };
+      alert("Successfully user created ");
+    }
+    else {
+      alert("Please fill all fields");
+    }
   }
 }
